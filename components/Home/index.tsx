53d70a25df1c75f6e02482189898d69a46795cd3
dungpham <me2015_dung.pq@student.vgu.edu.vn>
2021-05-22 16:31:59 +0700

    style color Home.module.scss

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -9,19 +9,19 @@ function Home() {
 			<div className={s.home}>
 				<h1>Chọn chủ đề bạn đang vướng bận</h1>
 				<div className="pageSubTitle">Hãy hít thở nhẹ trước khi chọn bài nhé</div>
-				<div className="box backgroundOrange">
+				<div className="box backgroundOrange boxShadowOrange">
 					<a href={"/career"}>
 						<h5 className="boxTitle">Sự nghiệp</h5>
 						<div className="boxSubTitle">Lời khuyên cho sự nghiệp của bạn</div>
 					</a>
 				</div>
-				<div className="box backgroundPurple">
+				<div className="box backgroundPurple boxShadowPurple">
 					<a href={"/feeling"}>
 					<h5 className="boxTitle">Tình cảm</h5>
 					<div className="boxSubTitle">Soi nghĩa, thấu hiểu những cảm nhận của bạn</div>
 					</a>
 				</div>
-				<div className="box backgroundGreen">
+				<div className="box backgroundGreen boxShadowGreen">
 					<a href="/random">
 						<h5 className="boxTitle">Ngẫu nhiên</h5>
 						<div className="boxSubTitle">Để sự tình cờ dẫn lối tới thông điệp vũ trụ</div>
