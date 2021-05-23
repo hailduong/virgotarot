05b36d2f38bc9781512fce426c00f0ffbb87f040
Dung Pham <me2015_dung.pq@student.vgu.edu.vn>
2021-05-23 20:29:39 +0700

    update career.tsx index.tsx

---
+++ components/Career/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { memo, useState } from 'react'
+
+function Career() {
+    return (
+        <div>
+            <div className="dung">
+                <div className={"container"}>
+                    <div className={"navigation"}><a href={"/"}>&lt;Chọn chủ đề</a></div>
+                    <div className={"endPage navigation"}>Rút thêm</div>
+                </div>
+                <h1 className={"bigTitle"}>Sự nghiệp</h1>
+                <img src="/images/major/0 the-fool.png" alt="Tarot card"/>
+                <div className={"tarotCardNumber upperCard"}>0.</div>
+                <div className={"major upperCard"}>Chàng khờ</div>
+                <div className={"minor"}>Tôi hào hứng với những cơ hội</div>
+                <div className={"minor endPage"}>Tôi muốn ra khỏi vùng an toàn</div>
+                <div className={"minor footNote1"}><i>*Các dự báo của tarot không xa hơn 1 năm</i></div>
+            </div>
+        </div>
+    );
+}
+
+export default memo(Career)
--- pages/career.tsx
+++ pages/career.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import React from 'react'
 import MasterLayout from "../components/globals/MasterLayout";
 import Card from "../components/Random/Card";
+import Career from "../components/Career"
 
 class Index extends React.PureComponent<any, any> {
 
@@ -21,19 +22,7 @@ class Index extends React.PureComponent<any, any> {
 
 			<main>
 				<MasterLayout>
-					<div className="dung">
-						<div className={"container"}>
-							<div className={"navigation"}><a href={"/"}>&lt;Chọn chủ đề</a></div>
-							<div className={"endPage navigation"}>Rút thêm</div>
-						</div>
-						<h1 className={"bigTitle"}>Sự nghiệp</h1>
-						<img src="/images/major/0 the-fool.png" alt="Tarot card"/>
-						<div className={"tarotCardNumber upperCard"}>0.</div>
-						<div className={"major upperCard"}>Chàng khờ</div>
-						<div className={"minor"}>Tôi hào hứng với những cơ hội</div>
-						<div className={"minor endPage"}>Tôi muốn ra khỏi vùng an toàn</div>
-						<div className={"minor footNote1"}><i>*Các dự báo của tarot không xa hơn 1 năm</i></div>
-					</div>
+					<Career />
 				</MasterLayout>
 			</main>
 
