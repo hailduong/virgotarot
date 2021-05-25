dc8d4e9f26840cdb5da86773eeee10e3826d8206
dungpham <me2015_dung.pq@student.vgu.edu.vn>
2021-05-25 21:50:31 +0700

    Update cardHelper.tsx

--- components/helpers/cardHelper.tsx
+++ components/helpers/cardHelper.tsx
@@ -1,20 +1,31 @@
-import { ALL_CARDS } from '../constants/data'
+import {ALL_CARDS} from '../constants/data'
 
 const imagePath = `/images`
 
 
 class CardHelper {
-	getRandomCardId(min, max): number {
-		return Math.round((Math.random() * (max - min) + min))
-	}
+    getRandomCardId(min, max): number {
+        return Math.round((Math.random() * (max - min) + min))
+    }
 
-	getCardData(cardId: number) {
-		return ALL_CARDS[cardId] || ALL_CARDS[0]
-	}
+    getCardData(cardId: number) {
+        return ALL_CARDS[cardId] || ALL_CARDS[0]
+    }
 
-	getCardPath(imageName: string) {
-		return `${imagePath}/major/${imageName}.png`
-	}
+    getCardPath(imageName: string) {
+        let path = "";
+        if (imageName.includes("cups")) {
+            path = `${imagePath}/minor/1 cups/${imageName}.png`
+        } else if (imageName.includes("swords")) {
+            path = `${imagePath}/minor/2 swords/${imageName}.png`
+        } else if (imageName.includes("wands")) {
+            path = `${imagePath}/minor/3 wands/${imageName}.png`
+        } else if (imageName.includes("pentacles")) {
+            path = `${imagePath}/minor/4 pentacles/${imageName}.png`
+        } else path=`${imagePath}/major/${imageName}.png`;
+
+        return path;
+    }
 }
 
 export default new CardHelper()
