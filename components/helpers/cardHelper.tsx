b9fc3171b29efef169344ada1cc89d4b76d1207e
LyDuong <ly.duong@navigosgroup.com>
2021-05-26 20:08:14 +0700

    update: minor code refactor

--- components/helpers/cardHelper.tsx
+++ components/helpers/cardHelper.tsx
@@ -1,31 +1,31 @@
-import {ALL_CARDS} from '../constants/data'
+import { ALL_CARDS } from '../constants/data'
 
 const imagePath = `/images`
 
 
 class CardHelper {
-    getRandomCardId(min, max): number {
-        return Math.round((Math.random() * (max - min) + min))
-    }
+	getRandomCardId(min, max): number {
+		return Math.round((Math.random() * (max - min) + min))
+	}
 
-    getCardData(cardId: number) {
-        return ALL_CARDS[cardId] || ALL_CARDS[0]
-    }
+	getCardData(cardId: number) {
+		return ALL_CARDS[cardId] || ALL_CARDS[0]
+	}
 
-    getCardPath(imageName: string) {
-        let path = "";
-        if (imageName.includes("cups")) {
-            path = `${imagePath}/minor/1 cups/${imageName}.png`
-        } else if (imageName.includes("swords")) {
-            path = `${imagePath}/minor/2 swords/${imageName}.png`
-        } else if (imageName.includes("wands")) {
-            path = `${imagePath}/minor/3 wands/${imageName}.png`
-        } else if (imageName.includes("pentacles")) {
-            path = `${imagePath}/minor/4 pentacles/${imageName}.png`
-        } else path=`${imagePath}/major/${imageName}.png`;
+	getCardPath(imageName: string) {
+		let path = `${imagePath}/major/${imageName}.png`
+		if (imageName.includes('cups')) {
+			path = `${imagePath}/minor/1_cups/${imageName}.png`
+		} else if (imageName.includes('swords')) {
+			path = `${imagePath}/minor/2_swords/${imageName}.png`
+		} else if (imageName.includes('wands')) {
+			path = `${imagePath}/minor/3_wands/${imageName}.png`
+		} else if (imageName.includes('pentacles')) {
+			path = `${imagePath}/minor/4_pentacles/${imageName}.png`
+		}
 
-        return path;
-    }
+		return path
+	}
 }
 
 export default new CardHelper()
